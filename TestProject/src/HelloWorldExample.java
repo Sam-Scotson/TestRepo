15e2c9ead864f18ce7ba08d3ca7f6f616e29148e
Sam Scotson <easttowest17@gmail.com>
2024-05-27 11:13:36 +1200

    Example commit

--- TestProject/src/HelloWorldExample.java
+++ TestProject/src/HelloWorldExample.java
@@ -5,7 +5,7 @@ public class HelloWorldExample {
  
      public static void main(String[] args) {
  
-         // TODO comment
+         // Output Hello World to the user
  
          System.out.println("Hello World");
  
